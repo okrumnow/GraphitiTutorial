0ec0814255b08a72829e964d636ade96b9ae0bd7
Olaf Krumnow <okrumnow@gmx.de>
2011-09-04 12:11:41 +0200

    added diagram type provider

---
+++ de.jebc.tutorial.graphiti/src/de/jebc/tutorial/graphiti/diagram/MyTutorialDiagramTypeProvider.java
@@ -0,0 +1,10 @@
+package de.jebc.tutorial.graphiti.diagram;
+
+import org.eclipse.graphiti.dt.AbstractDiagramTypeProvider;
+
+public class MyTutorialDiagramTypeProvider extends AbstractDiagramTypeProvider {
+
+	public MyTutorialDiagramTypeProvider() {
+	}
+
+}
